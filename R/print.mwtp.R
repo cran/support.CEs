a5363eb1bcfec8991734b4764220c989c9ce5288
Hideo Aizaki <aizaki@affrc.go.jp>
2012-06-14 00:00:00 +0000

    version 0.2-3

--- R/print.mwtp.R
+++ R/print.mwtp.R
@@ -1,7 +1,8 @@
 print.mwtp <- function(x, digits = max(3, getOption("digits") - 3), scientific = FALSE, ...)
 {
   cat("\n")
-  print(format(x$mwtp.table, digits = digits, scientific = scientific, ...), quote = FALSE)
+  print(format(x$mwtp.table, digits = digits, scientific = scientific, ...), 
+        quote = FALSE, right = TRUE)
   cat("\n")
 
   invisible(x)
--- R/questionnaire.R
+++ R/questionnaire.R
@@ -2,34 +2,35 @@ questionnaire <-
 function(choice.experiment.design)
 {
 # Assign design information
-    nblocks <- choice.experiment.design$design.information$nblocks
-    nquestions <- choice.experiment.design$design.information$nquestions
-    nalternatives <- choice.experiment.design$design.information$nalternatives
-    nattributes <- choice.experiment.design$design.information$nattributes
+  nblocks <- choice.experiment.design$design.information$nblocks
+  nquestions <- choice.experiment.design$design.information$nquestions
+  nalternatives <- choice.experiment.design$design.information$nalternatives
+  nattributes <- choice.experiment.design$design.information$nattributes
 
 # Integrate alternatives into data.frame
-    my.design <- as.matrix(choice.experiment.design[[1]][[1]])
-    for (i in 2:nalternatives) {
-        my.design <- rbind(my.design, as.matrix(choice.experiment.design[[1]][[i]]))
-    }
-    rownames(my.design) <- NULL 
-    my.design <- data.frame(my.design)
-    my.design$BLOCK <- as.numeric(as.character(my.design$BLOCK))
-    my.design$QES <- as.numeric(as.character(my.design$QES))
-    my.design$ALT <- as.numeric(as.character(my.design$ALT))
+  my.design <- as.matrix(choice.experiment.design[[1]][[1]])
+  for (i in 2:nalternatives) {
+    my.design <- rbind(my.design,
+                       as.matrix(choice.experiment.design[[1]][[i]]))
+  }
+  rownames(my.design) <- NULL 
+  my.design <- data.frame(my.design)
+  my.design$BLOCK <- as.numeric(as.character(my.design$BLOCK))
+  my.design$QES <- as.numeric(as.character(my.design$QES))
+  my.design$ALT <- as.numeric(as.character(my.design$ALT))
 
 # Convert the choice experiment design into questions
-    my.design <- my.design[order(my.design$BLOCK, my.design$QES, my.design$ALT),]
-    alternative.names <- paste("alt.", 1:nalternatives, sep= "")
-    for (i in 1:nblocks) {
-        cat("Block", i, "\n", "\n")
-        for (j in 1:nquestions) {
-            cat("Question", j, "\n")
-            temp <- t(subset(subset(my.design, BLOCK == i), QES == j)[, 4:(3 + nattributes)])
-            colnames(temp) <- alternative.names
-            print(temp)
-            cat("\n")
-        }
+  my.design <- my.design[order(my.design$BLOCK, my.design$QES, my.design$ALT),]
+  alternative.names <- paste("alt.", 1:nalternatives, sep= "")
+  for (i in 1:nblocks) {
+    cat("Block", i, "\n", "\n")
+    for (j in 1:nquestions) {
+      cat("Question", j, "\n")
+      temp <- t(subset(my.design, my.design$BLOCK == i & my.design$QES == j)[, 4:(3 + nattributes)])
+      colnames(temp) <- alternative.names
+      print(temp)
+      cat("\n")
     }
+  }
 }
 
